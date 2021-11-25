459edbd443597ba01dd09887a3f787230b04463a
praveenkad <kadambaripraveen@gmail.com>
2021-11-26 02:35:59 +0530

    Added new TestSuite class

---
+++ src/test/java/com/testsuite/httpclient/SampleTest.java
@@ -0,0 +1,12 @@
+package com.testsuite.httpclient;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void runMe() {
+	System.out.println("Hey I'm a new file");
+	}
+
+}
